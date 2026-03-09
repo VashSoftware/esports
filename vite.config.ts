cf802d37a95c1a1b12937869be905f6939ee8a38
Stan Runge <stan@stanrunge.dev>
2026-03-09 20:59:58 +0100

    mark bun as external rollup option

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
 	ssr: {
 		external: ['bun']
 	},
+	build: {
+		rollupOptions: {
+			external: ['bun']
+		}
+	},
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
