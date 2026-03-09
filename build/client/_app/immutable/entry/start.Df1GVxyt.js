1697a53f162e07b5977debab757bf2421a95ce20
Stan Runge <stan@stanrunge.dev>
2026-03-09 21:01:23 +0100

    delete build folder from repo

--- build/client/_app/immutable/entry/start.Df1GVxyt.js
+++
@@ -1,1 +0,0 @@
-import{c as s}from"../chunks/BkIq08el.js";import{B as t}from"../chunks/DAtZQVjv.js";export{t as load_css,s as start};
--- build/server/chunks/aggregate-CrXc04Dg.js
+++
@@ -1,8 +0,0 @@
-import { x as sql } from './index2-B7hVh_Qf.js';
-
-function count(expression) {
-  return sql`count(${sql.raw("*")})`.mapWith(Number);
-}
-
-export { count as c };
-//# sourceMappingURL=aggregate-CrXc04Dg.js.map
